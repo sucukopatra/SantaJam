ce26ebd112a2f6a1c7df8d2099208ddc75a285d0
kaanthedesigner <acipkaan@gmail.com>
2025-12-27 05:44:09 +0300

    ada
    ad

--- Assets/Scripts/Gun.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class Gun : MonoBehaviour
-{
-}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] public Transform spawnPoint; // Baþýna [SerializeField] ekledik
+    [SerializeField] public GameObject karakter;
 
     void Start()
     {
+        if (PlayerMovement.Instance == null) { Instantiate(karakter); }
+
         if (LevelTeleporter.cameFromTransition && PlayerMovement.Instance != null)
         {
+            Debug.Log("AAAAAAAAAAAAAA");
+            if (karakter) karakter.SetActive(false);
             PlayerMovement.Instance.transform.position = spawnPoint.position;
             LevelTeleporter.cameFromTransition = false;
         }
