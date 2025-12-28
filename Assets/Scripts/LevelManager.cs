815e936c2cf7c9910325af427ed6e542510ae4ce
sucukopatra <endercortuk@proton.me>
2025-12-28 15:58:16 +0300

    ev eklendi

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,26 +1,20 @@
 using UnityEngine;
-using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] GameObject spawnPoint;
+    [SerializeField] Transform teleportPoint;
     [SerializeField] GameObject player;
-
 
     private void Start()
     {
         if (PlayerMovement.Instance != null)
         {
-            PlayerMovement.Instance.transform.position = spawnPoint.transform.position;
-            StartCoroutine(FalseJustTeleported());
+            PlayerMovement.Instance.transform.position = teleportPoint.transform.position;
         }
         else
         {
             Instantiate(player);
         }
     }
-    IEnumerator FalseJustTeleported(){
-        yield return new WaitForSeconds(1);
-        PlayerMovement.Instance.justTeleported = false;
-    }
+    
 }
